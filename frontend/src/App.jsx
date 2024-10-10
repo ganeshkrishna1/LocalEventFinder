a6cffac5d100a21d1387a022156522487b35d102
vidyadharchalla24 <vidyadharchalla333@gmail.com>
2024-10-10 14:20:44 +0530

    landingpage done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import SignUp from './pages/signup/SignUp'
 import SignIn from './pages/signin/SignIn'
+import HeroSection from './components/home/HeroSection'
+import AboutUs from './components/aboutus/AboutUs'
+import FAQ from './components/FAQ/FAQ'
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
     <>
       <Navbar />
       <Routes>
+        <Route path='/' element={<HeroSection />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/frequently-asked-questions' element={<FAQ />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
       </Routes>
